cb640a06ac51ce99236422dde6d85bcb736baafd
Eugene <yovlinpentdragonvii@gmail.com>
2024-03-27 13:40:36 +0300

    index.js

--- index.js
+++ index.js
@@ -1,2 +1,19 @@
 
-// Write your code here
+let num1 = 31;
+let num2 = 2;
+let multiply = num1 * num2;
+
+let random = Math.ceil(Math.random() * 100); // Generates a random integer between 1 and 100
+
+
+let num3 = 64;
+let num4 = 60;
+let mod = num3 % num4;
+
+//  variable named max that finds the highest number in a set and the value returned should be 20.
+let max = Math.max(5, 10, 15, 20);
+
+console.log("multiply:", multiply); 
+console.log("random:", random); 
+console.log("mod:", mod); 
+console.log("max:", max); 
